c4b2e8160ac7418cfe19730714ad1f095b56077c
Tharindu Kasthurisinghe <tharindukasthurisinghe@gmail.com>
2024-09-22 19:41:39 +0530

    Improve the GUI

--- src/gui.py
+++ src/gui.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog, messagebox
-from tkinter.ttk import Progressbar, Label, Entry, Button, Style, Frame
+from tkinter.ttk import Progressbar, Entry, Button, Style, Frame
 import threading
 import os
 from .processing import bulk_rename_images
@@ -10,7 +10,7 @@ class BulkRenameApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Bulk Rename Images")
-        self.root.geometry("1050x680")
+        self.root.geometry("700x630")
         self.style = Style()
         self.style.configure("TLabel", font=("Poppins", 12), background="#f0f0f0")
         self.style.configure(
@@ -21,10 +21,15 @@ class BulkRenameApp:
         self.create_widgets()
 
     def create_widgets(self):
+        # Configure grid columns to stretch
+        self.root.columnconfigure(0, weight=1)
+        self.root.columnconfigure(1, weight=1)
+        self.root.columnconfigure(2, weight=1)
+
         # Title
         title_frame = Frame(self.root, padding="10", style="TFrame")
-        title_frame.grid(row=0, column=0, columnspan=3, pady=10)
-        title_label = Label(
+        title_frame.grid(row=0, column=0, columnspan=3, pady=10, sticky="ew")
+        title_label = tk.Label(
             title_frame,
             text="Bulk Rename Images",
             font=("Poppins", 16, "bold"),
@@ -34,69 +39,67 @@ class BulkRenameApp:
 
         # Source folder
         source_frame = Frame(self.root, padding="10", style="TFrame")
-        source_frame.grid(row=1, column=0, columnspan=3, pady=5, sticky="w")
-        Label(source_frame, text="Source Folder:", background="#f0f0f0").grid(
-            row=0, column=0, padx=10, pady=5, sticky="w"
-        )
-        self.source_folder_entry = Entry(source_frame, width=70)
-        self.source_folder_entry.grid(row=0, column=1, padx=10, pady=5, ipady=5)
+        source_frame.grid(row=1, column=0, columnspan=3, pady=5, sticky="ew")
+        source_frame.columnconfigure(0, weight=1)
+        self.source_folder_entry = Entry(source_frame)
+        self.source_folder_entry.grid(row=0, column=0, padx=10, pady=5, sticky="ew")
+        self.source_folder_entry.insert(0, "Source Folder")
         Button(
             source_frame,
             text="Browse",
             command=lambda: self.browse_folder(self.source_folder_entry),
-        ).grid(row=0, column=2, padx=10, pady=5)
+        ).grid(row=0, column=1, padx=10, pady=5)
 
         # Destination folder
         destination_frame = Frame(self.root, padding="10", style="TFrame")
-        destination_frame.grid(row=2, column=0, columnspan=3, pady=5, sticky="w")
-        Label(destination_frame, text="Destination Folder:", background="#f0f0f0").grid(
-            row=0, column=0, padx=10, pady=5, sticky="w"
+        destination_frame.grid(row=2, column=0, columnspan=3, pady=5, sticky="ew")
+        destination_frame.columnconfigure(0, weight=1)
+        self.destination_folder_entry = Entry(destination_frame)
+        self.destination_folder_entry.grid(
+            row=0, column=0, padx=10, pady=5, sticky="ew"
         )
-        self.destination_folder_entry = Entry(destination_frame, width=70)
-        self.destination_folder_entry.grid(row=0, column=1, padx=10, pady=5, ipady=5)
+        self.destination_folder_entry.insert(0, "Destination Folder")
         Button(
             destination_frame,
             text="Browse",
             command=lambda: self.browse_folder(self.destination_folder_entry),
-        ).grid(row=0, column=2, padx=10, pady=5)
+        ).grid(row=0, column=1, padx=10, pady=5)
 
         # Main name
         main_name_frame = Frame(self.root, padding="10", style="TFrame")
-        main_name_frame.grid(row=3, column=0, columnspan=3, pady=5, sticky="w")
-        Label(main_name_frame, text="Main Name:", background="#f0f0f0").grid(
-            row=0, column=0, padx=10, pady=5, sticky="w"
-        )
-        self.main_name_entry = Entry(main_name_frame, width=70)
-        self.main_name_entry.grid(row=0, column=1, padx=10, pady=5, ipady=5)
+        main_name_frame.grid(row=3, column=0, columnspan=3, pady=5, sticky="ew")
+        main_name_frame.columnconfigure(0, weight=1)
+        self.main_name_entry = Entry(main_name_frame)
+        self.main_name_entry.grid(row=0, column=0, padx=10, pady=5, sticky="ew")
+        self.main_name_entry.insert(0, "Main Name")
 
         # Start number
         start_number_frame = Frame(self.root, padding="10", style="TFrame")
-        start_number_frame.grid(row=4, column=0, columnspan=3, pady=5, sticky="w")
-        Label(start_number_frame, text="Start Number:", background="#f0f0f0").grid(
-            row=0, column=0, padx=10, pady=5, sticky="w"
-        )
-        self.start_number_entry = Entry(start_number_frame, width=70)
-        self.start_number_entry.grid(row=0, column=1, padx=10, pady=5, ipady=5)
+        start_number_frame.grid(row=4, column=0, columnspan=3, pady=5, sticky="ew")
+        start_number_frame.columnconfigure(0, weight=1)
+        self.start_number_entry = Entry(start_number_frame)
+        self.start_number_entry.grid(row=0, column=0, padx=10, pady=5, sticky="ew")
+        self.start_number_entry.insert(0, "Start Number")
 
         # Progress bar
         progress_frame = Frame(self.root, padding="10", style="TFrame")
-        progress_frame.grid(row=5, column=0, columnspan=3, pady=20)
+        progress_frame.grid(row=5, column=0, columnspan=3, pady=20, sticky="ew")
         self.progress_bar = Progressbar(
-            progress_frame, orient=tk.HORIZONTAL, length=1000, mode="determinate"
+            progress_frame, orient=tk.HORIZONTAL, length=600, mode="determinate"
         )
-        self.progress_bar.pack()
+        self.progress_bar.pack(fill="x")
 
         # Status label
         status_frame = Frame(self.root, padding="10", style="TFrame")
-        status_frame.grid(row=6, column=0, columnspan=3, pady=5)
-        self.status_label = Label(
+        status_frame.grid(row=6, column=0, columnspan=3, pady=5, sticky="ew")
+        self.status_label = tk.Label(
             status_frame, text="", font=("Poppins", 10), background="#f0f0f0"
         )
         self.status_label.pack()
 
         # Start button
         start_button_frame = Frame(self.root, padding="10", style="TFrame")
-        start_button_frame.grid(row=7, column=0, columnspan=3, pady=20)
+        start_button_frame.grid(row=7, column=0, columnspan=3, pady=20, sticky="ew")
         Button(start_button_frame, text="Start", command=self.start_bulk_rename).pack()
 
     def browse_folder(self, entry):
